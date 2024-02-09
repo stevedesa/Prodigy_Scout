3b4049bc58277ad18d826379ccaa299cd84dc9ff
Steve de Sa <steve.desa@mines.sdsmt.edu>
2024-02-09 15:24:20 +0000

    Revert "Merge branch '1-build-initial-template-project' into 'master'"
    This reverts merge request !2

--- Team_Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Team_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
