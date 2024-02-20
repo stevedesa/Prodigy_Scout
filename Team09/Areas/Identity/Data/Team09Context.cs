78bd6560c67931168f8040b4c386d4ed25af8d40
Steve Nathan de Sa <stevenathandesa@gmail.com>
2024-02-20 02:43:36 -0700

    Ref #5 Added Identity Attributes for Authentication and User Login/Reg

---
+++ Team09/Areas/Identity/Data/Team09Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Team09.Data;
+
+public class Team09Context : IdentityDbContext<IdentityUser>
+{
+    public Team09Context(DbContextOptions<Team09Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
